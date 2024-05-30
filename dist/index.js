daffb8d9b69571d5c0b928ee0a74cc31d2b18494
StudioWavves <studiowavves@gmail.com>
2024-05-29 20:59:16 -0400

    feat: added base cart system package

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Cart = void 0;
+var Cart_1 = require("./Cart");
+Object.defineProperty(exports, "Cart", { enumerable: true, get: function () { return __importDefault(Cart_1).default; } });
---
+++ src/Cart.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cart = () => {
+    return (
+        <div>
+            THIS IS THE UNIVERSAL CART SYSTEM
+        </div>
+    )
+}
+
+export default Cart;
